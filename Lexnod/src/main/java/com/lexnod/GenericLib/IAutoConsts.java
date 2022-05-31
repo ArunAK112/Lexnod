6d6ad8d43c01ec5215587a7854bb2f0620168e5b
ARUN <ARUN@LAPTOP-3AI9QJI2>
2022-05-31 15:52:46 +0530

    Project Commit

---
+++ Lexnod/src/main/java/com/lexnod/GenericLib/IAutoConsts.java
@@ -0,0 +1,16 @@
+package com.lexnod.GenericLib;
+
+public interface IAutoConsts {
+
+	String CHROME_KEY="webdriver.chrome.driver";
+	String CHROME_VALUE="./src/main/resources/drivers/chromedriver.exe";
+	
+	String GECKO_KEY="webdriver.gecko.driver";
+	String GECKO_VALUE="./src/main/resources/drivers/geckodriver.exe";
+	
+	String PROP_PATH="./src/test/resources/dataRepo/config.properties";
+	String EXCEL_PATH="./src/test/resources/dataRepo/inputData.xlsx";
+	
+	String REPORT_PATH = "./customReports/LexnodResult.html";
+	
+}
